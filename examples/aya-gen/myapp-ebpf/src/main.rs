9d38e890611780f0436d17677d7051bbac51490d
Michal Rostecki <mrostecki@opensuse.org>
2021-11-19 15:13:29 +0100

    examples: Add eBPF LSM program which is using BTF/aya-gen
    This program is going to be an example of aya-gen usage. It uses a
    vmlinux.rs module with task_struct and its dependencies. The program is
    attaching to the task_alloc LSM hook and saving PIDs of new processes in
    the map.
    
    Signed-off-by: Michal Rostecki <mrostecki@opensuse.org>

---
+++ examples/aya-gen/myapp-ebpf/src/main.rs
@@ -0,0 +1,50 @@
+#![no_std]
+#![no_main]
+
+#[allow(non_upper_case_globals)]
+#[allow(non_snake_case)]
+#[allow(non_camel_case_types)]
+#[allow(dead_code)]
+mod vmlinux;
+
+use aya_bpf::{
+    cty::{c_int, c_ulong},
+    macros::{lsm, map},
+    maps::HashMap,
+    programs::LsmContext,
+};
+
+use vmlinux::task_struct;
+
+#[map]
+static mut PROCESSES: HashMap<i32, i32> = HashMap::with_max_entries(32768, 0);
+
+#[lsm(name = "task_alloc")]
+pub fn task_alloc(ctx: LsmContext) -> i32 {
+    match unsafe { try_task_alloc(ctx) } {
+        Ok(ret) => ret,
+        Err(ret) => ret,
+    }
+}
+
+unsafe fn try_task_alloc(ctx: LsmContext) -> Result<i32, i32> {
+    let task: *const task_struct = ctx.arg(0);
+    let _clone_flags: c_ulong = ctx.arg(1);
+    let retval: c_int = ctx.arg(2);
+
+    // Save the PID of a new process in map.
+    let pid = (*task).pid;
+    PROCESSES.insert(&pid, &pid, 0).map_err(|e| e as i32)?;
+
+    // Handle results of previous LSM programs.
+    if retval != 0 {
+        return Ok(retval);
+    }
+
+    Ok(0)
+}
+
+#[panic_handler]
+fn panic(_info: &core::panic::PanicInfo) -> ! {
+    unsafe { core::hint::unreachable_unchecked() }
+}
---
+++ examples/aya-gen/myapp/src/main.rs
@@ -0,0 +1,55 @@
+use aya::{include_bytes_aligned, Bpf};
+use aya::{programs::Lsm, Btf};
+use std::{
+    convert::{TryFrom, TryInto},
+    sync::atomic::{AtomicBool, Ordering},
+    sync::Arc,
+    thread,
+    time::Duration,
+};
+use structopt::StructOpt;
+
+fn main() {
+    if let Err(e) = try_main() {
+        eprintln!("error: {:#}", e);
+    }
+}
+
+#[derive(Debug, StructOpt)]
+struct Opt {}
+
+fn try_main() -> Result<(), anyhow::Error> {
+    let opt = Opt::from_args();
+    // This will include youe eBPF object file as raw bytes at compile-time and load it at
+    // runtime. This approach is recommended for most real-world use cases. If you would
+    // like to specify the eBPF program at runtime rather than at compile-time, you can
+    // reach for `Bpf::load_file` instead.
+    #[cfg(debug_assertions)]
+    let mut bpf = Bpf::load(include_bytes_aligned!(
+        "../../target/bpfel-unknown-none/debug/myapp"
+    ))?;
+    #[cfg(not(debug_assertions))]
+    let mut bpf = Bpf::load(include_bytes_aligned!(
+        "../../target/bpfel-unknown-none/release/myapp"
+    ))?;
+    let btf = Btf::from_sys_fs()?;
+    let program: &mut Lsm = bpf.program_mut("task_alloc")?.try_into()?;
+    program.load("task_alloc", &btf)?;
+    program.attach()?;
+
+    let running = Arc::new(AtomicBool::new(true));
+    let r = running.clone();
+
+    ctrlc::set_handler(move || {
+        r.store(false, Ordering::SeqCst);
+    })
+    .expect("Error setting Ctrl-C handler");
+
+    println!("Waiting for Ctrl-C...");
+    while running.load(Ordering::SeqCst) {
+        thread::sleep(Duration::from_millis(500))
+    }
+    println!("Exiting...");
+
+    Ok(())
+}
