bb5932b25d8c61982f2e03b3964fdfc0d4d20922
Michal Rostecki <vadorovsky@gmail.com>
2022-10-18 23:26:27 +0200

    examples: Rename myapp-* projects to xdp-*
    Make the names of example XDP projects descriptive.
    
    Signed-off-by: Michal Rostecki <vadorovsky@gmail.com>

---
+++ examples/xdp-log/xdp-log/src/main.rs
@@ -0,0 +1,75 @@
+use aya::{include_bytes_aligned, Bpf};
+use anyhow::Context;
+use aya::programs::{Xdp, XdpFlags};
+use aya::maps::perf::AsyncPerfEventArray;
+use aya::util::online_cpus;
+use bytes::BytesMut;
+use std::net;
+use clap::Parser;
+use log::info;
+use tokio::{signal, task};
+
+use xdp_log_common::PacketLog;
+
+#[derive(Debug, Parser)]
+struct Opt {
+    #[clap(short, long, default_value = "eth0")]
+    iface: String,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), anyhow::Error> {
+    let opt = Opt::parse();
+
+    env_logger::init();
+
+    // This will include your eBPF object file as raw bytes at compile-time and load it at
+    // runtime. This approach is recommended for most real-world use cases. If you would
+    // like to specify the eBPF program at runtime rather than at compile-time, you can
+    // reach for `Bpf::load_file` instead.
+    #[cfg(debug_assertions)]
+    let mut bpf = Bpf::load(include_bytes_aligned!(
+        "../../target/bpfel-unknown-none/debug/xdp-log"
+    ))?;
+    #[cfg(not(debug_assertions))]
+    let mut bpf = Bpf::load(include_bytes_aligned!(
+        "../../target/bpfel-unknown-none/release/xdp-log"
+    ))?;
+    // (1)
+    let program: &mut Xdp = bpf.program_mut("xdp").unwrap().try_into()?;
+    program.load()?;
+    program.attach(&opt.iface, XdpFlags::default())
+        .context("failed to attach the XDP program with default flags - try changing XdpFlags::default() to XdpFlags::SKB_MODE")?;
+
+    // (2)
+    let mut perf_array = AsyncPerfEventArray::try_from(bpf.map_mut("EVENTS")?)?;
+
+    for cpu_id in online_cpus()? {
+        // (3)
+        let mut buf = perf_array.open(cpu_id, None)?;
+
+        // (4)
+        task::spawn(async move {
+            // (5)
+            let mut buffers = (0..10)
+                .map(|_| BytesMut::with_capacity(1024))
+                .collect::<Vec<_>>();
+
+            loop {
+                // (6)
+                let events = buf.read_events(&mut buffers).await.unwrap();
+                for i in 0..events.read {
+                    let buf = &mut buffers[i];
+                    let ptr = buf.as_ptr() as *const PacketLog;
+                    // (7)
+                    let data = unsafe { ptr.read_unaligned() };
+                    let src_addr = net::Ipv4Addr::from(data.ipv4_address);
+                    // (8)
+                    info!("LOG: SRC {}, ACTION {}", src_addr, data.action);
+                }
+            }
+        });
+    }
+    signal::ctrl_c().await.expect("failed to listen for event");
+    Ok::<_, anyhow::Error>(())
+}
