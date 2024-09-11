e17ef3d2adf15ef42f26f68db864016e4fa862b7
Andras Gemes <andrasgemes@outlook.com>
2024-09-11 14:52:10 +0200

    Update deprecated names

--- examples/kprobetcp/kprobetcp/src/main.rs
+++ examples/kprobetcp/kprobetcp/src/main.rs
@@ -1,5 +1,5 @@
-use aya::{include_bytes_aligned, programs::KProbe, Bpf};
-use aya_log::BpfLogger;
+use aya::{include_bytes_aligned, programs::KProbe, Ebpf};
+use aya_log::EbpfLogger;
 use clap::Parser;
 use log::{info, warn};
 use tokio::signal;
@@ -16,16 +16,16 @@ async fn main() -> Result<(), anyhow::Error> {
     // This will include your eBPF object file as raw bytes at compile-time and load it at
     // runtime. This approach is recommended for most real-world use cases. If you would
     // like to specify the eBPF program at runtime rather than at compile-time, you can
-    // reach for `Bpf::load_file` instead.
+    // reach for `Ebpf::load_file` instead.
     #[cfg(debug_assertions)]
-    let mut bpf = Bpf::load(include_bytes_aligned!(
+    let mut bpf = Ebpf::load(include_bytes_aligned!(
         "../../target/bpfel-unknown-none/debug/kprobetcp"
     ))?;
     #[cfg(not(debug_assertions))]
-    let mut bpf = Bpf::load(include_bytes_aligned!(
+    let mut bpf = Ebpf::load(include_bytes_aligned!(
         "../../target/bpfel-unknown-none/release/kprobetcp"
     ))?;
-    if let Err(e) = BpfLogger::init(&mut bpf) {
+    if let Err(e) = EbpfLogger::init(&mut bpf) {
         // This can happen if you remove all log statements from your eBPF program.
         warn!("failed to initialize eBPF logger: {e}");
     }
