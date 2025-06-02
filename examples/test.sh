bb5d705a5ab554a3d6730f19fc2cfb5b69fc72b8
Tamir Duberstein <tamird@gmail.com>
2025-06-02 13:51:45 -0400

    Use nightly clippy, run over entire workspace

--- examples/test.sh
+++ examples/test.sh
@@ -10,20 +10,10 @@ for dir in *; do
   pushd "${dir}"
 
   cargo +nightly fmt --check
-  cargo build
-  cargo build --release
-  bpf_crate=$dir-ebpf
-  if [ "${dir}" == "aya-tool" ]; then
-    bpf_crate=myapp-ebpf
-  fi
+  cargo +nightly clippy "$@" --workspace --all-targets -- --deny warnings -C panic=abort -Zpanic_abort_tests
 
-  # We can't run clippy over the entire workspace all at once because we need panic=abort for the
-  # ${bpf_crate} crate.
-  #
-  # We can't use --all-targets on ${bpf_crate} because building tests with panic=abort isn't
-  # supported without -Zpanic_abort_tests.
-  cargo clippy --exclude "${bpf_crate}" --all-targets --workspace -- --deny warnings
-  cargo clippy --package "${bpf_crate}" -- --deny warnings -C panic=abort
+  cargo build "$@"
+  cargo build "$@" --release
 
   popd
 done
