d966f0aff34b7cc0980a4d9177c62a195d738e7f
Tamir Duberstein <tamird@gmail.com>
2025-06-03 06:07:58 -0400

    Fix build under new cargo_metadata

--- examples/xdp-hello/xdp-hello/build.rs
+++ examples/xdp-hello/xdp-hello/build.rs
@@ -9,7 +9,9 @@ fn main() -> anyhow::Result<()> {
             .context("MetadataCommand::exec")?;
     let ebpf_package = packages
         .into_iter()
-        .find(|cargo_metadata::Package { name, .. }| name == "xdp-hello-ebpf")
+        .find(|cargo_metadata::Package { name, .. }| {
+            name.as_str() == "xdp-hello-ebpf"
+        })
         .ok_or_else(|| anyhow!("xdp-hello-ebpf package not found"))?;
     aya_build::build_ebpf([ebpf_package], Toolchain::default())
 }
