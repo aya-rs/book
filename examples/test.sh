68bedceedbb86b8cd23603d0d4241c49a4a2cebb
Tamir Duberstein <tamird@gmail.com>
2025-06-27 15:30:44 -0400

    Simplify clippy invocation
    We don't need `--all-targets` here; we don't have any tests.

--- examples/test.sh
+++ examples/test.sh
@@ -11,18 +11,9 @@ for dir in *; do
 
   cargo +nightly fmt --check
 
-  bpf_crate=$dir-ebpf
-  if [ "${dir}" == "aya-tool" ]; then
-    bpf_crate=myapp-ebpf
-  fi
-
-  # We can't run clippy over the entire workspace all at once because we need panic=abort for the
-  # ${bpf_crate} crate.
-  #
-  # We can't use --all-targets on ${bpf_crate} because building tests with panic=abort isn't
-  # supported without -Zpanic_abort_tests.
-  cargo clippy "$@" --exclude "${bpf_crate}" --all-targets --workspace -- --deny warnings
-  cargo clippy "$@" --package "${bpf_crate}" -- --deny warnings -C panic=abort
+  # `-C panic=abort` because "unwinding panics are not supported without std"; ebpf programs are
+  # `#[no_std]` binaries.
+  cargo clippy "$@" -- --deny warnings -C panic=abort
 
   popd
 done
