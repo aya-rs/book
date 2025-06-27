9e56373436cc9fd53cdb44d0fb41ed087a92bf86
Tamir Duberstein <tamird@gmail.com>
2025-06-27 13:05:22 -0400

    Allow passing arguments to test.sh
    This makes it possible to run `./test.sh --target
    x86_64-unknown-linux-gnu` on macOS.

--- examples/test.sh
+++ examples/test.sh
@@ -10,8 +10,8 @@ for dir in *; do
   pushd "${dir}"
 
   cargo +nightly fmt --check
-  cargo build
-  cargo build --release
+  cargo build "$@"
+  cargo build "$@" --release
   bpf_crate=$dir-ebpf
   if [ "${dir}" == "aya-tool" ]; then
     bpf_crate=myapp-ebpf
@@ -22,8 +22,8 @@ for dir in *; do
   #
   # We can't use --all-targets on ${bpf_crate} because building tests with panic=abort isn't
   # supported without -Zpanic_abort_tests.
-  cargo clippy --exclude "${bpf_crate}" --all-targets --workspace -- --deny warnings
-  cargo clippy --package "${bpf_crate}" -- --deny warnings -C panic=abort
+  cargo clippy "$@" --exclude "${bpf_crate}" --all-targets --workspace -- --deny warnings
+  cargo clippy "$@" --package "${bpf_crate}" -- --deny warnings -C panic=abort
 
   popd
 done
