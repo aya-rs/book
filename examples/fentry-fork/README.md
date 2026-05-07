b3ec0519ee0afdfe111e2c84cfb0d660285704c0
Nikita Meshcheriakov <root@nikitam.io>
2026-05-07 10:54:39 -0400

    fentry example

---
+++ examples/fentry-fork/README.md
@@ -0,0 +1,34 @@
+# fentry-fork
+
+A test eBPF Aya-based Rust program that attaches fentry and fexit handlers
+to `kernel_clone` and prints the PID of the process that creates a new
+process and the PID of the newly created process (excluding thread
+creation).
+
+## Prerequisites
+
+1. Install a rust stable toolchain: `rustup install stable`
+1. Install a rust nightly toolchain: `rustup install nightly`
+1. Install bpf-linker: `cargo install bpf-linker`
+
+## Build & Run
+
+Use `cargo build`, `cargo check`, etc. as normal. Run your program with:
+
+```shell
+RUST_LOG=info cargo run --config 'target."cfg(all())".runner="sudo -E"'
+```
+
+Run any command that creates a new process in a different terminal, such as
+`ls` or `bash -c true`. The expected output (the PIDs will likely be
+different):
+
+<!-- markdownlint-disable MD013 -->
+
+```console
+[2022-12-28T20:50:00Z INFO  fentry_fork] Waiting for Ctrl-C...
+[2022-12-28T20:50:05Z INFO  fentry_fork] Process creation is started by: 12345
+[2022-12-28T20:50:05Z INFO  fentry_fork] New process is created by: 12345 child id: 67890
+```
+
+<!-- markdownlint-enable MD013 -->
---
+++ examples/fentry-fork/fentry-fork-ebpf/src/main.rs
@@ -0,0 +1,63 @@
+#![no_std]
+#![no_main]
+#![allow(linker_messages)]
+
+use aya_ebpf::helpers::bpf_get_current_pid_tgid;
+use aya_ebpf::macros::{fentry, fexit};
+use aya_ebpf::programs::{FEntryContext, FExitContext};
+use aya_log_ebpf::info;
+
+#[allow(
+    clippy::all,
+    dead_code,
+    improper_ctypes_definitions,
+    non_camel_case_types,
+    non_snake_case,
+    non_upper_case_globals,
+    unnecessary_transmutes,
+    unsafe_op_in_unsafe_fn,
+)]
+#[rustfmt::skip]
+mod vmlinux;
+
+use vmlinux::kernel_clone_args;
+
+const CLONE_THREAD: u64 = 0x00010000;
+
+#[fentry]
+pub fn fentry_clone(ctx: FEntryContext) -> u32 {
+    let args_ptr: *const kernel_clone_args = ctx.arg(0);
+    let flags = unsafe { (*args_ptr).flags };
+
+    if flags & CLONE_THREAD == 0 {
+        info!(
+            &ctx,
+            "Process creation is started by: {}",
+            bpf_get_current_pid_tgid() as u32
+        );
+    }
+    0
+}
+
+#[fexit]
+pub fn fexit_clone(ctx: FExitContext) -> u32 {
+    let args_ptr: *const kernel_clone_args = ctx.arg(0);
+    let flags = unsafe { (*args_ptr).flags };
+    let return_value: i32 = ctx.arg(1);
+
+    if flags & CLONE_THREAD == 0 {
+        info!(
+            &ctx,
+            "New process is created by: {} child id: {}",
+            bpf_get_current_pid_tgid() as u32,
+            return_value
+        );
+    }
+    0
+}
+
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(_info: &core::panic::PanicInfo) -> ! {
+    loop {}
+}
